d420976ff309d79219d41a01b65b5214949981c7
Daniel Gruno <humbedooh@apache.org>
2020-10-20 17:14:28 +0200

    OF: Check for dict, not list here

--- modules/gitbox/files/asfgit/asfyaml.py
+++ modules/gitbox/files/asfgit/asfyaml.py
@@ -433,7 +433,7 @@ def github(cfg, yml):
         # These branches will be stripped of its protection settings at the end.
         enabledProtectedBranches = getEnabledProtectedBranchList(GH_TOKEN, repo_name, False, False)
 
-        if isinstance(protected_branches, list) and all(isinstance(x, basestring) for x in protected_branches):
+        if isinstance(protected_branches, dict) and all(isinstance(x, basestring) for x in protected_branches):
             # For each defined branch, fetch and format the user-defined settings and submit GH API.
             for pb_branch in protected_branches:
                 pb_branch_data = protected_branches.get(pb_branch)
