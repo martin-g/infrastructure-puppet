0b19a980701c2ec361266cdf6d15136b3e37b2e9
Daniel Gruno <humbedooh@apache.org>
2020-12-07 15:28:52 +0100

    Sync stageD with p6

--- modules/staged/files/staged.py
+++ modules/staged/files/staged.py
@@ -19,21 +19,44 @@ ROOT_DIR = '/www'
 # Staging on staging-vm, publishing on tlp-* boxes
 PUBLISH = True if 'tlp' in socket.gethostname() else False
 
+
 def checkout_git_repo(path, source, branch):
     """ Checks out a new staging/publish site from a repo """
+    os.chdir(ROOT_DIR)
+    # If dir already exists as a dir, delete it first
+    if os.path.isdir(path):
+        syslog.syslog(syslog.LOG_INFO, "Doing recursive delete of path %s" % path)
+        shutil.rmtree(path)
+    # Try to pull in the repo, if it fails, just log it for now.
     syslog.syslog(syslog.LOG_INFO, "Checking out %s (%s) as %s..." % (source, branch, path))
     try:
-        rv = subprocess.check_output((GIT_CMD, "clone", "-b", branch, "--single-branch", source, path), stderr=subprocess.STDOUT).decode('utf-8')
-        syslog.syslog(syslog.LOG_INFO, "Checkout worked! Git said:")
-        for line in rv.split("\n"):
-            syslog.syslog(syslog.LOG_INFO, "[git %s] %s" % (source, line))
+        subprocess.check_output((GIT_CMD, "clone", "-b", branch, "--single-branch", source, path))
+        syslog.syslog(syslog.LOG_INFO, "Checkout worked!")
     except subprocess.CalledProcessError as e:
         syslog.syslog(syslog.LOG_WARNING, "Could not check out %s: %s" % (source, e.output))
-    
+
+
+def do_git_pull(path, branch):
+    """Does a simple git pull from a deploy dir, syslog if it works or not"""
+    os.chdir(path)
+    try:
+        subprocess.check_output((GIT_CMD, 'pull', 'origin', branch))
+        syslog.syslog(syslog.LOG_INFO, "Successfully completed `git pull` into %s" % path)
+    except subprocess.CalledProcessError as e:
+        syslog.syslog(syslog.LOG_WARNING, "Could not `git pull` new contents into %s: %s" % (path, e.output))
+        # If we hit a merge conflict due to rewritten history (detached tree), try a reset.
+        if b"Merge conflict" in e.output or b"refusing to merge unrelated histories" in e.output:
+            syslog.syslog(syslog.LOG_INFO, "%s has merge conflicts or orphaned head, trying to reset the repository." % path)
+            try:
+                subprocess.check_output((GIT_CMD, 'reset', '--hard', 'origin/%s' % branch))
+                syslog.syslog(syslog.LOG_INFO, "%s was successfully reset." % path)
+            except subprocess.CalledProcessError as e:
+                syslog.syslog(syslog.LOG_WARNING, "Could not `git reset --hard` in %s, giving up: %s" % (path, e.output))
+
 
 def deploy_site(deploydir, source, branch, committer):
     """ Deploys a git repo to a staging/live site """
-    
+
     # Pre-validation:
     if deploydir == 'www.apache.org':
         syslog.syslog(syslog.LOG_WARNING, "Not going to touch www.a.o, nope!!" % deploydir)
@@ -47,7 +70,7 @@ def deploy_site(deploydir, source, branch, committer):
     if not branch:
         syslog.syslog(syslog.LOG_WARNING, "Invalid branch, %s!" % branch)
         return
-    
+
     # First check if staging dir is already being used.
     # If it is, check if we can just do a git pull...
     path = os.path.join(ROOT_DIR, deploydir)
@@ -57,37 +80,38 @@ def deploy_site(deploydir, source, branch, committer):
         try:
             csource = subprocess.check_output((GIT_CMD, 'config', '--get', 'remote.origin.url')).decode('utf-8').strip()
             cbranch = subprocess.check_output((GIT_CMD, 'symbolic-ref', '--short', 'HEAD')).decode('utf-8').strip()
-            # If different repo, clobber dir and re-checkout
-            if csource != source:
-                syslog.syslog(syslog.LOG_INFO, "Source repo for %s is not %s (%s), clobbering repo." % (path, source, csource))
-                os.chdir(ROOT_DIR)
-                shutil.rmtree(path)
-                checkout_git_repo(path, source, branch)
-            # Or if different branch, switch
-            elif cbranch != branch:
-                syslog.syslog(syslog.LOG_INFO, "Source branch for %s is not %s, switching branches." % (path, branch))
-                subprocess.check_output((GIT_CMD, 'stash')) # Juuust in case
-                subprocess.check_output((GIT_CMD, 'fetch', 'origin', branch))
-                subprocess.check_output((GIT_CMD, 'checkout', '-b', branch, 'FETCH_HEAD'))
-                subprocess.check_output((GIT_CMD, 'pull'))
-            # Or it could be all good, just needs a pull
-            else:
-                syslog.syslog(syslog.LOG_INFO, "Source and branch match on-disk, doing git pull")
-                subprocess.check_output((GIT_CMD, 'pull', 'origin', branch))
         except subprocess.CalledProcessError as e:
-            syslog.syslog(syslog.LOG_WARNING, "Could not determine original source of %s, clobbering: %s" % (path, e.output))
-            os.chdir(ROOT_DIR)
-            shutil.rmtree(path)
+            syslog.syslog(syslog.LOG_WARNING,
+                          "Could not determine original source of %s, clobbering: %s" % (path, e.output))
+
             checkout_git_repo(path, source, branch)
+            return
+        # If different repo, clobber dir and re-checkout
+        if csource != source:
+            syslog.syslog(syslog.LOG_INFO,
+                          "Source repo for %s is not %s (%s), clobbering repo." % (path, source, csource))
+            checkout_git_repo(path, source, branch)
+        # Or if different branch, switch
+        elif cbranch != branch:
+            syslog.syslog(syslog.LOG_INFO, "Source branch for %s is not %s, switching branches." % (path, branch))
+            subprocess.check_output((GIT_CMD, 'stash'))  # Juuust in case
+            subprocess.check_output((GIT_CMD, 'fetch', 'origin', branch))
+            subprocess.check_output((GIT_CMD, 'checkout', '-b', branch, 'FETCH_HEAD'))
+            do_git_pull(path, branch)
+        # Or it could be all good, just needs a pull
+        else:
+            syslog.syslog(syslog.LOG_INFO, "Source and branch match on-disk, doing git pull")
+            do_git_pull(path, branch)
     # Otherwise, do fresh checkout
     else:
         syslog.syslog(syslog.LOG_INFO, "%s is a new staging dir, doing fresh checkout" % path)
         checkout_git_repo(path, source, branch)
 
+
 class deploy(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
-    
+
     def run(self):
         """ Copy queue, clear it and run each item """
         global PUBSUB_QUEUE
@@ -100,6 +124,7 @@ class deploy(threading.Thread):
                 except Exception as e:
                     syslog.syslog(syslog.LOG_WARNING, "BORK: Could not deploy to %s: %s" % (deploydir, e))
             time.sleep(5)
+
 
 def read_chunk(req):
     """ Processor func for reading "lines" (http chunks) """
@@ -115,6 +140,7 @@ def read_chunk(req):
             break
     return
 
+
 def listen():
     """ PubSub listener """
     last = time.time()
@@ -127,12 +153,13 @@ def listen():
                 req = urllib.request.urlopen(PUBSUB_URL, None, 30)
                 syslog.syslog(syslog.LOG_INFO, "Subscribed, reading stream")
             except:
-                syslog.syslog(syslog.LOG_WARNING, "Could not connect to pubsub service at %s, retrying in 30s..." % PUBSUB_URL)
+                syslog.syslog(syslog.LOG_WARNING,
+                              "Could not connect to pubsub service at %s, retrying in 30s..." % PUBSUB_URL)
                 time.sleep(30)
                 continue
-            
+
         for line in read_chunk(req):
-            line = line.decode('utf-8', errors='ignore' ).rstrip('\r\n,').replace('\x00','')
+            line = line.decode('utf-8', errors='ignore').rstrip('\r\n,').replace('\x00', '')
             try:
                 what = 'staging' if not PUBLISH else 'publish'
                 obj = json.loads(line)
@@ -143,7 +170,7 @@ def listen():
                     profile = obj[what].get('profile', '')
                     committer = obj[what].get('pusher', 'root')
                     subdir = obj[what].get('subdir', '')
-                    
+
                     # Staging dir
                     deploydir = project
                     if profile:
@@ -156,18 +183,21 @@ def listen():
                             hostdir = obj[what].get('target')
                             if hostdir:
                                 deploydir = hostdir  # Only if not empty value
-                    
+
                     if (deploydir and source and branch):
-                        if subdir and re.match(r"^[-_a-zA-Z0-9/]+$", subdir):                            
-                            syslog.syslog(syslog.LOG_INFO, "Extending deployment dir %s with subdir %s" % (deploydir, subdir))
+                        if subdir and re.match(r"^[-_a-zA-Z0-9/]+$", subdir):
+                            syslog.syslog(syslog.LOG_INFO,
+                                          "Extending deployment dir %s with subdir %s" % (deploydir, subdir))
                             deploydir = os.path.join(deploydir, subdir)
-                        syslog.syslog(syslog.LOG_INFO, "Found deploy delivery for %s, deploying as %s" % (project, deploydir))
+                        syslog.syslog(syslog.LOG_INFO,
+                                      "Found deploy delivery for %s, deploying as %s" % (project, deploydir))
                         PUBSUB_QUEUE[deploydir] = [source, branch, committer]
-                    
+
             except ValueError as detail:
                 syslog.syslog(syslog.LOG_WARNING, "Bad JSON or something: %s" % detail)
                 continue
         syslog.syslog(syslog.LOG_WARNING, "Disconnected from %s, reconnecting" % PUBSUB_URL)
+
 
 if __name__ == '__main__':
     deployer = deploy()
